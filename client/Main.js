6f09c1ce96eeb9225335e711bf3c18c86e84fa22
Jane An <janeyuhwian@gmail.com>
2020-06-14 17:59:56 -0400

    allalbums player sidebar react

--- client/Main.js
+++ client/Main.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import Player from './Player'
+import AllAlbums from './AllAlbums'
 
 export default class Main extends React.Component {
   render () {
     return (
       <div id='main' className='row container'>
-        {/* The music starts here! */}
+        <Sidebar />
+        <AllAlbums />
+        <Player />
       </div>
     )
   }
---
+++ client/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Sidebar() {
+    return (
+        <div>
+            <div id='sidebar'>
+                <img src='juke.svg' id='logo' />
+                <section>
+                    <h4>
+                        <a>ALBUMS</a>
+                    </h4>
+                </section>
+            </div>
+        </div>
+    )
+}
+
